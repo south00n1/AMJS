67a77dc8def60b20795fb04948334261f9651409
sbe03 <sbe03@hj>
2023-01-12 17:46:03 +0900

    전시회정보VO 생성

---
+++ TempProject/src/main/java/com/sist/vo/ExhibitionVO.java
@@ -0,0 +1,115 @@
+package com.sist.vo;
+
+public class ExhibitionVO {
+	private int geno,good;
+	private String poster,title,title2,kind,period,loc,loc2,area,are2,item,host,url,price,time,hastag,content;
+	public int getGeno() {
+		return geno;
+	}
+	public void setGeno(int geno) {
+		this.geno = geno;
+	}
+	public int getGood() {
+		return good;
+	}
+	public void setGood(int good) {
+		this.good = good;
+	}
+	public String getPoster() {
+		return poster;
+	}
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getTitle2() {
+		return title2;
+	}
+	public void setTitle2(String title2) {
+		this.title2 = title2;
+	}
+	public String getKind() {
+		return kind;
+	}
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
+	public String getPeriod() {
+		return period;
+	}
+	public void setPeriod(String period) {
+		this.period = period;
+	}
+	public String getLoc() {
+		return loc;
+	}
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+	public String getLoc2() {
+		return loc2;
+	}
+	public void setLoc2(String loc2) {
+		this.loc2 = loc2;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public String getAre2() {
+		return are2;
+	}
+	public void setAre2(String are2) {
+		this.are2 = are2;
+	}
+	public String getItem() {
+		return item;
+	}
+	public void setItem(String item) {
+		this.item = item;
+	}
+	public String getHost() {
+		return host;
+	}
+	public void setHost(String host) {
+		this.host = host;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getHastag() {
+		return hastag;
+	}
+	public void setHastag(String hastag) {
+		this.hastag = hastag;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
