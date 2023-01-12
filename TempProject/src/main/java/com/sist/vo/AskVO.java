8c9d61e9c50210975ba6820b16e02aa0ffa14757
user <user@DESKTOP-COJ9858>
2023-01-12 17:43:31 +0900

    고객센터VO

---
+++ TempProject/src/main/java/com/sist/vo/AskVO.java
@@ -0,0 +1,92 @@
+package com.sist.vo;
+import java.util.*;
+
+public class AskVO {
+	private int gano,hit,group_id,group_step,group_tab;
+	private String subject,type,content,pwd,ans_state,msg,dbday,id;
+	private Date regdate;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getDbday() {
+		return dbday;
+	}
+	public void setDbday(String dbday) {
+		this.dbday = dbday;
+	}
+	public int getGano() {
+		return gano;
+	}
+	public void setGano(int gano) {
+		this.gano = gano;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	public int getGroup_id() {
+		return group_id;
+	}
+	public void setGroup_id(int group_id) {
+		this.group_id = group_id;
+	}
+	public int getGroup_step() {
+		return group_step;
+	}
+	public void setGroup_step(int group_step) {
+		this.group_step = group_step;
+	}
+	public int getGroup_tab() {
+		return group_tab;
+	}
+	public void setGroup_tab(int group_tab) {
+		this.group_tab = group_tab;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getAns_state() {
+		return ans_state;
+	}
+	public void setAns_state(String ans_state) {
+		this.ans_state = ans_state;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+}
---
+++ TempProject/src/main/java/com/sist/vo/FaqVO.java
@@ -0,0 +1,36 @@
+package com.sist.vo;
+
+public class FaqVO {
+	private int gfno,hit;
+	private String type,subject,content;
+	public int getGfno() {
+		return gfno;
+	}
+	public void setGfno(int gfno) {
+		this.gfno = gfno;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
